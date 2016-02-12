4f2ab774044d1ce0b97165e41756dc09bfcb83d2
mincong <i@congm.in>
2016-02-12 17:30:27 +0800

    update

--- index/js/main.js
+++ index/js/main.js
@@ -1,27 +1,52 @@
-
 var myLoading = loadImage('https://congm.in/bing.php');
 function loadImage(url){
 	var o = new Image();
 	o.src = url;
-    if(o.success)
-    {
-        $('#loading').fadeOut();
-        $('#loading').empty();
-        $(".wallpaper").attr('src',o.src);
-    }
-    else
-    {
+    if(o.success){
+    }else{
         o.onload = function(){
-            $('#loading').fadeOut();
-            $('#loading').empty();
-            $(".wallpaper").attr('src',o.src);
+            showImage(o.src);
         }
-        o.onerror = function(){  
-    		$(".wallpaper").attr('src','index/img/bg.jpg');
+        o.onerror = function(){
+            showImage('index/img/bg.jpg');
     	};
     }
 }
-
+var iUp = (function() {
+    var t = 0,
+        d = 150,
+        clean = function() {
+            t = 0;
+        },
+        up = function(e) {
+            setTimeout(function() {
+                $(e).addClass("up")
+            }, t);
+            t += d;
+        },
+        down = function(e) {
+            $(e).removeClass("up");
+        },
+        toggle = function(e){
+            setTimeout(function() {
+                $(e).toggleClass("up")
+            }, t);
+            t += d;
+        };
+    return {
+        clean: clean,
+        up: up,
+        down: down,
+        toggle: toggle
+    }
+})();
+function showImage(url){
+    $('#loading').fadeOut().empty();
+    $(".wallpaper").attr('src',url);
+    $(".iUp").each(function(i, e) {
+        iUp.up(e);
+    });
+}
 //随滚动变化
 $(window).scroll(function(){
     var height = $(window).height();
@@ -41,12 +66,13 @@ $(window).scroll(function(){
 
 $(function(){
     setTimeout(function(){
-        $(".wallpaper").attr('src',"https://congm.in/bing.php");
-        $('#loading').fadeOut();
-        $('#loading').empty();
+        showImage('https://congm.in/bing.php');
     },3000);
     $("#downTo").click(function(){
-        $("html,body").stop(true).animate({scrollTop:$("#second-screen").offset().top}, 2000);
+        $("html,body").stop(true).animate({
+            scrollTop: $("#second-screen").offset().top
+        }, 2000);
     });
 });
+
 
