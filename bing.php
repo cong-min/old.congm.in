0c75e77a54f6693c720b88df6e5c0d62269305de
mincong <i@congm.in>
2016-04-06 17:49:40 +0800

    update

--- bing.php
+++ bing.php
@@ -5,6 +5,9 @@
 	}
 	if($imgurl){
 		$img = imagecreatefromjpeg($imgurl);
+		header('Expires: ' . gmdate('D, d M Y H:i:s', strtotime(date('Y-m-d', strtotime('+1 day')))) . ' GMT'); 
+		header('Cache-Control: public, max-age=3600');
+		header('Last-Modified: ' . gmdate('D, d M Y H:i:s', strtotime(date('Y-m-d'))) . ' GMT'); 
 		header('Content-Type: image/jpeg');
 		imageinterlace($img, 1);
 		imagejpeg($img);
