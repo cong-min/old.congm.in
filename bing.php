e395c3bb40c3e0418572b7e96096d1beca195b52
Cong Min <i@congm.in>
2017-10-01 10:05:40 +0800

    update

--- bing.php
+++ bing.php
@@ -1,17 +1,12 @@
 <?php
-	$str = file_get_contents('http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1');
+	$str = file_get_contents('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1');
 	$data = json_decode($str);
-	$imgurl = $data -> {"images"}[0] -> {"url"};
-	if($imgurl){
-		$img = imagecreatefromjpeg($imgurl);
-		header('Expires: ' . gmdate('D, d M Y H:i:s', strtotime(date('Y-m-d', strtotime('+1 day')))) . ' GMT'); 
-		header('Cache-Control: public, max-age=3600');
-		header('Last-Modified: ' . gmdate('D, d M Y H:i:s', strtotime(date('Y-m-d'))) . ' GMT'); 
-		header('Content-Type: image/jpeg');
-		imageinterlace($img, 1);
-		imagejpeg($img);
-		imagedestroy($img);
-		exit();
+	$imghost = 'https://www.bing.com';
+	$imgpath = $data -> {"images"}[0] -> {"url"};
+	if($imgpath){
+	  $imgurl = $imghost . $imgpath;
+	  header('Location:'.$imgurl);
+	  exit();
 	}else{
 		exit('error');
 	}
